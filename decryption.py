49138bac093cf572989aa021d299a32070f341cf
Meet540 <152309022+Meet540@users.noreply.github.com>
2023-11-28 22:07:58 +0530

    Add files via upload

---
+++ decryption.py
@@ -0,0 +1,29 @@
+def doublealpha(alphabet):
+   doublealpha=alphabet+alphabet
+   return doublealpha  
+
+def getcipherkey():
+    shift=input("enter key(number between 1 to 25)")
+    return shift
+
+def getmsg():
+    ste=input("enter message to encrypt: ")
+    return ste
+
+def decryption(msg,cipherkey,alphabet):
+    decrytedtxt=""
+    uppertxt=""
+    uppertxt=msg.upper()
+    for word in uppertxt:
+      position=alphabet.find(word)
+      newposition=position-int(cipherkey)
+      if word in uppertxt:
+          decrytedtxt=decrytedtxt+alphabet[newposition]
+      else:
+          decrytedtxt=decrytedtxt+word
+    return decrytedtxt
+
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+c=getcipherkey()
+m=getmsg()
+print(decryption(m,c,alphabet))
