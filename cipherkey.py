3ff44424ef6e76728e75be29597b654e4d8d88bf
Logan-Punisher <jjoh6418@gmail.com>
2023-11-29 16:28:19 +0530

    cipher-key modification

--- cipherkey.py
+++ cipherkey.py
@@ -1,13 +1,12 @@
 def getCipherkey():
-    while True:
-        try:
-            sa = input("Please enter a key (whole number from 1-25): ")
-            sa = int(sa)
-            if 1<= sa <=25:
-                return sa
-            else:
-                print("enter Numeric value")
-        except ValueError:
-            print("You have entered Non-numeric")
-
-
+    key = input("Enter the crypto-key Number between 1 - 26 to encrypt: ")
+    if key.isdigit():
+        key = int(key)
+        if 0 < key < 26:
+            return key
+        else:
+            print("Enter the value between 1 - 26 to encrypt !!!!!")
+            return getCipherkey()
+    else:
+        print("Entered Crypto-key is not numeric Kindly redo :-(")
+        return getCipherkey()
